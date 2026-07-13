3c7fd35d372d2e9ec7f129b55444a4036f19448d
kimhonghue <ghdfo720@naver.com>
2026-07-14 07:21:55 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,8 @@
 let liveChatId = "";
 let lastMessageId = "";
+
 let direction = true;
+let dogNumber = 1;
 
 
 async function getLiveChatId() {
@@ -14,76 +16,100 @@ async function getLiveChatId() {
   liveChatId = data.items[0].liveStreamingDetails.activeLiveChatId;
 
   getMessages();
+
 }
 
 
+
 async function getMessages() {
+
 
   const url =
   `https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${liveChatId}&part=snippet&key=${API_KEY}`;
 
 
   const response = await fetch(url);
+
   const data = await response.json();
 
 
-  if(data.items.length > 0){
+
+  if(data.items && data.items.length > 0){
+
 
     const message = data.items[data.items.length - 1];
 
 
     if(message.id !== lastMessageId){
 
+
       lastMessageId = message.id;
+
 
       createPet(message.snippet.displayMessage);
 
+
     }
+
   }
 
 
+
   setTimeout(getMessages,3000);
+
 
 }
 
 
 
+
 function createPet(text){
 
+
   const pet = document.createElement("div");
-  pet.className = "pet";
+
+  pet.className = direction ? "pet right" : "pet left";
+
 
 
   const bubble = document.createElement("div");
+
   bubble.className = "bubble";
+
   bubble.innerText = text;
 
 
+
   const dog = document.createElement("img");
-  dog.src="dog1.png";
-  dog.className="dog";
+
+  dog.src = `dog${dogNumber}.png`;
+
+  dog.className = "dog";
 
 
-  pet.appendChild(bubble);
-  pet.appendChild(dog);
 
-  document.body.appendChild(pet);
+  dogNumber++;
 
+  if(dogNumber > 9){
 
-  if(direction){
-
-    pet.style.left="-200px";
-    pet.style.animation="moveRight 10s linear forwards";
-
-  } else {
-
-    pet.style.right="-200px";
-    pet.style.animation="moveLeft 10s linear forwards";
+    dogNumber = 1;
 
   }
 
 
-  direction=!direction;
+
+
+  pet.appendChild(bubble);
+
+  pet.appendChild(dog);
+
+
+  document.body.appendChild(pet);
+
+
+
+  direction = !direction;
+
 
 }
 
