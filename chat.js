f0f3e395b2fce6b40304b3723a4dc15265731a20
kimhonghue <ghdfo720@naver.com>
2026-07-14 07:04:14 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,6 +3,7 @@ let lastMessageId = "";
 
 
 async function getLiveChatId() {
+
   const response = await fetch(
     `https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=${VIDEO_ID}&key=${API_KEY}`
   );
@@ -20,6 +21,7 @@ async function getMessages() {
   const url =
     `https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${liveChatId}&part=snippet,authorDetails&key=${API_KEY}`;
 
+
   const response = await fetch(url);
   const data = await response.json();
 
@@ -28,41 +30,61 @@ async function getMessages() {
 
     const newest = data.items[data.items.length - 1];
 
+
     if (newest.id !== lastMessageId) {
 
       lastMessageId = newest.id;
 
+
+      // 채팅 글 가져오기
       showDog(newest.snippet.displayMessage);
+
     }
+
   }
 
 
   setTimeout(getMessages, 3000);
+
 }
 
 
-function showDog(message){
 
-  const box = document.createElement("div");
-  box.className = "chatBox";
-  box.innerText = message;
+function showDog(message) {
+
+
+  const chatBox = document.createElement("div");
+
+  chatBox.className = "chatBox";
+
+  chatBox.innerText = message;
+
 
 
   const dog = document.createElement("img");
+
   dog.src = "dog1.png";
+
   dog.className = "dog";
 
 
-  document.body.appendChild(box);
+
+  document.body.appendChild(chatBox);
+
   document.body.appendChild(dog);
 
 
-  setTimeout(()=>{
-    box.remove();
+
+  setTimeout(() => {
+
+    chatBox.remove();
+
     dog.remove();
+
   },5000);
 
 }
+
 
 
 getLiveChatId();
