c8276a0386ff2f4db320f4406ec8bedd7a40d2f7
kimhonghue <ghdfo720@naver.com>
2026-07-14 04:57:07 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,2 +1,37 @@
-const API_KEY = AIzaSyA51WTjni-pTNiP14TMwCrXj_3MnWEdWlw
-const VIDEO_ID = jbulx7qli6
+async function getChatId(){
+
+  const res = await fetch(
+    `https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=${VIDEO_ID}&key=${API_KEY}`
+  );
+
+  const data = await res.json();
+
+  return data.items[0]
+    .liveStreamingDetails
+    .activeLiveChatId;
+}
+
+
+async function readChat(){
+
+  const chatId = await getChatId();
+
+  const res = await fetch(
+    `https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${chatId}&part=snippet,authorDetails&key=${API_KEY}`
+  );
+
+  const data = await res.json();
+
+  data.items.forEach(msg=>{
+
+    createDog(
+      msg.authorDetails.displayName,
+      msg.snippet.displayMessage
+    );
+
+  });
+
+}
+
+
+setInterval(readChat,5000);
